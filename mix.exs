19ea2a987a837be8dc934b697d93d52d2395de30
maneptha <maneptha@gmail.com>
2018-07-11 21:52:57 +0200

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,22 @@
+defmodule Blackjack.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :blackjack,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [mod: {Blackjack, []}, extra_applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+end
